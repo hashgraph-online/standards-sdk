94e47b63401980c08b015e46c172fe30055b2a1f
Michael Kantor <6068672+kantorcodes@users.noreply.github.com>
2025-12-29 15:50:29 -0500

    feat: add xmpt demos
    Signed-off-by: Michael Kantor <6068672+kantorcodes@users.noreply.github.com>

---
+++ demo/utils/local-xmpt-agent.ts
@@ -0,0 +1,6 @@
+export type {
+  LocalXmtpAgentHandle as LocalXmptAgentHandle,
+  LocalXmtpAgentOptions as LocalXmptAgentOptions,
+} from './local-xmtp-agent';
+
+export { startLocalXmtpAgent as startLocalXmptAgent } from './local-xmtp-agent';
